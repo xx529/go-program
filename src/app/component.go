bf72f2a8500e02155f07d16ccf2642ad59bbefbb
Hang <64747715+xx529@users.noreply.github.com>
2023-10-28 22:21:26 +0800

    add struct

--- src/app/component.go
+++ src/app/component.go
@@ -8,6 +8,52 @@ import (
 	"runtime"
 )
 
+type pageElements struct {
+	progressBar       *widget.ProgressBar // 进度条
+	progressText      *widget.Label       // 进度条文字提示
+	statusText        *widget.Label       // 状态提示
+	dockerVersionInfo *widget.Label       // docker 版本信息
+	dockerComposeInfo *widget.Label       // docker compose 版本信息
+	buttonInstall     *widget.Button      // 安装按钮
+}
+
+func newPageElements() *pageElements {
+	p := pageElements{
+		progressBar:       widget.NewProgressBar(),
+		progressText:      widget.NewLabel(""),
+		statusText:        widget.NewLabel(""),
+		dockerVersionInfo: widget.NewLabel("checking docker version...\n"),
+		dockerComposeInfo: widget.NewLabel("checking docker compose version...\n"),
+		buttonInstall:     widget.NewButton("Install", nil),
+	}
+	p.progressBar.Min = 0
+	p.progressBar.Max = 1
+	return &p
+}
+
+func (p *pageElements) layout() *fyne.Container {
+
+	// 按钮水平布局
+	horizontalLayout := container.NewCenter(p.buttonInstall)
+
+	// 垂直布局
+	content := container.NewVBox(
+		widget.NewLabel(""),
+		horizontalLayout,
+		widget.NewLabel(""),
+		p.progressBar,
+		widget.NewLabel(""),
+		container.NewCenter(p.progressText),
+		container.NewCenter(p.statusText),
+		widget.NewLabel(""),
+		widget.NewLabel(""),
+		container.NewCenter(widget.NewLabel("OS: "+runtime.GOOS)),
+		container.NewCenter(widget.NewLabel("ARCH: "+runtime.GOARCH)),
+		container.NewCenter(p.dockerVersionInfo),
+	)
+	return content
+}
+
 func RunApp() {
 
 	// 创建和启动应用
