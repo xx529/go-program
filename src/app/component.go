9bc798019cdf60b24d904b634dc4b02ea5ba0db6
Hang <64747715+xx529@users.noreply.github.com>
2023-10-29 23:33:01 +0800

    update struct

--- src/app/component.go
+++ src/app/component.go
@@ -31,7 +31,7 @@ func newPageElements() *pageElements {
 	return &p
 }
 
-func (p *pageElements) layout() *fyne.Container {
+func (p *pageElements) createLayout() *fyne.Container {
 
 	// 按钮水平布局
 	horizontalLayout := container.NewCenter(p.buttonInstall)
@@ -61,52 +61,58 @@ func RunApp() {
 	myWindow := myApp.NewWindow(WindowName)
 	myWindow.Resize(fyne.NewSize(WindowWidth, WindowHeight))
 
+	// 创建所有元素
+	elements := newPageElements()
+
 	// 创建进度条
 	progressBar := widget.NewProgressBar()
 	progressBar.Min = 0
 	progressBar.Max = 1
 
-	// 进度条文字提示
-	progressText := widget.NewLabel("")
-
-	// 状态提示
-	statusText := widget.NewLabel("")
-
-	// docker 信息
-	dockerText := widget.NewLabel("checking docker version...\n")
-	dockerVersion(dockerText)
-
-	// 安装按钮
-	buttonInstall := widget.NewButton("Install",
-		installFunc(statusText, progressBar, progressText),
-	)
+	//// 进度条文字提示
+	//progressText := widget.NewLabel("")
+	//
+	//// 状态提示
+	//statusText := widget.NewLabel("")
+	//
+	//// docker 信息
+	//dockerText := widget.NewLabel("checking docker version...\n")
+	//dockerVersion(dockerText)
+	//
+	//// 安装按钮
+	//buttonInstall := widget.NewButton("Install",
+	//	installFunc(statusText, progressBar, progressText),
+	//)
 
 	// 布局
-	content := layout(buttonInstall, progressBar, progressText, statusText, dockerText)
+	//content := layout(buttonInstall, progressBar, progressText, statusText, dockerText)
+
+	// 布局
+	content := elements.createLayout()
 
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 }
 
-func layout(buttonInstall *widget.Button, progressBar *widget.ProgressBar, progressText *widget.Label, statusText *widget.Label, dockerText *widget.Label) *fyne.Container {
-
-	// 按钮水平布局
-	horizontalLayout := container.NewCenter(buttonInstall)
-
-	// 垂直布局
-	content := container.NewVBox(
-		widget.NewLabel(""),
-		horizontalLayout,
-		widget.NewLabel(""),
-		progressBar,
-		widget.NewLabel(""),
-		container.NewCenter(progressText),
-		container.NewCenter(statusText),
-		widget.NewLabel(""),
-		widget.NewLabel(""),
-		container.NewCenter(widget.NewLabel("OS: "+runtime.GOOS)),
-		container.NewCenter(widget.NewLabel("ARCH: "+runtime.GOARCH)),
-		container.NewCenter(dockerText),
-	)
-	return content
-}
+//func layout(buttonInstall *widget.Button, progressBar *widget.ProgressBar, progressText *widget.Label, statusText *widget.Label, dockerText *widget.Label) *fyne.Container {
+//
+//	// 按钮水平布局
+//	horizontalLayout := container.NewCenter(buttonInstall)
+//
+//	// 垂直布局
+//	content := container.NewVBox(
+//		widget.NewLabel(""),
+//		horizontalLayout,
+//		widget.NewLabel(""),
+//		progressBar,
+//		widget.NewLabel(""),
+//		container.NewCenter(progressText),
+//		container.NewCenter(statusText),
+//		widget.NewLabel(""),
+//		widget.NewLabel(""),
+//		container.NewCenter(widget.NewLabel("OS: "+runtime.GOOS)),
+//		container.NewCenter(widget.NewLabel("ARCH: "+runtime.GOARCH)),
+//		container.NewCenter(dockerText),
+//	)
+//	return content
+//}
