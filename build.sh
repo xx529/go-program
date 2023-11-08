7d46a19318b7e2294c31783930831229cdc5c46c
Hang <64747715+xx529@users.noreply.github.com>
2023-11-09 01:54:58 +0800

    new dockerop

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-go build -o ./dist/app ./src/main.go
+# 编译 windows 版本
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o ./dist/main.exe ./src/main.go
+
+#go build -o ./dist/main ./src/main.go
--- src/app/variables.go
+++ src/app/variables.go
@@ -6,3 +6,14 @@ const (
 	WindowHeight = 350
 	RunningUrl   = "http://127.0.0.1:13011"
 )
+
+const (
+	SupportSystem           = "windows"
+	DefaultDockerDesktopDir = "C:\\Program Files\\Docker\\Docker\\"
+	DefaultExeFileName      = "Docker Desktop.exe"
+	DefaultDockerDesktopExe = DefaultDockerDesktopDir + DefaultExeFileName
+)
+
+const (
+	DockerOpTimeOutSeconds = 120
+)
